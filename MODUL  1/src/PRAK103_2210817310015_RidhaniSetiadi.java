5d4c41a6025585b87a30add8b35fa1f79f1b2cad
RidhaniSetiadi <114981190+RidhaniSetiadi@users.noreply.github.com>
2023-09-18 19:18:58 +0800

    Add files via upload

---
+++ MODUL  1/src/PRAK103_2210817310015_RidhaniSetiadi.java
@@ -0,0 +1,13 @@
+public class PRAK103_2210817310015_RidhaniSetiadi {
+    public static void main(String[] args) {
+        System.out.print("############             ##\n");
+        System.out.print("#          #             ##\n");
+        System.out.print("#          #             ##\n");
+        System.out.print("#        #               ##\n");
+        System.out.print("#      #                 ##\n");
+        System.out.print("# # #                    ##\n");
+        System.out.print("#     #                  ##\n");
+        System.out.print("#       #                ##\n");
+        System.out.print("#         #              ##\n");
+    }
+}
